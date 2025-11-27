5b641276ec467b2680d6cf7f237e18aa2976addd
Luiz <alvesluizeduardo23@gmail.com>
2025-11-27 13:36:26 -0300

    Mudando caminho do modelo

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ async function main() {
   // compiles and links the shaders, looks up attribute and uniform locations
   const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
 
-  const response = await fetch('https://alvesluiz23.github.io/ghost_blue.obj');
+  const response = await fetch('https://alvesluiz23.github.io/ProjetoCG/ghost_blue.obj');
   const text = await response.text();
   const data = parseOBJ(text);
 
